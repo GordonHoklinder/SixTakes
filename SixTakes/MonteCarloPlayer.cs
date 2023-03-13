f4cfefe9f5d4fe2b999d409e0d05d29820b4dfe9
GordonHoklinder <ondra.sladky@gmail.com>
2023-03-13 13:06:35 +0100

    Speed up of choosing remaining cards.

--- SixTakes/MonteCarloPlayer.cs
+++ SixTakes/MonteCarloPlayer.cs
@@ -10,19 +10,41 @@ namespace SixTakes
     {
         Random rng = new Random();
 
-        protected List<int> ChooseRemainingCards(Game game, int count)
+        protected List<int> ChooseRemainingCardsBySorting(Game game, int count)
         {
+            List<bool> present = new List<bool>(new bool [105]);
             List<int> result = new List<int>();
-            // TODO this is too slow.
-            while(result.Count != count)
+            for (int card = 1; card < 105; card++)
             {
-                int x = rng.Next(1, 105);
-                if (! result.Contains(x) && !game.History.Contains(x)) result.Add(x);
+                if (!result.Contains(card) && !game.History.Contains(card)) result.Add(card);
             }
-            return result;  
+            return result.OrderBy(x => rng.Next()).Take(count).ToList();
         }
 
-        protected (int, int) MonteCarlo(List<int> hand, Game game, int depth, int iterations = 100)
+        protected List<int> ChooseRemainingCardsRandomly(Game game, int count)
+        {
+            List<int> result = new List<int>();
+            while (result.Count != count)
+            {
+                int x = rng.Next(1, 105);
+                if (!result.Contains(x) && !game.History.Contains(x)) result.Add(x);
+            }
+            return result;
+        }
+
+        protected List<int> ChooseRemainingCards(Game game, int count)
+        {
+            int countPlusHistory = count + game.History.Count;
+            // If the expected number of operations of randomly probing values exceeds
+            // estimated number of operations of sorting, choose the cards via sorting.
+            if (count * countPlusHistory * (1 - ((double)countPlusHistory) / 105) > 630)
+            {
+                return ChooseRemainingCardsBySorting(game, count);
+            }
+            return ChooseRemainingCardsRandomly(game, count);           
+        }
+
+        protected (int, int) MonteCarlo(List<int> hand, Game game, int depth, int iterations = 200)
         {
             // Stores the highest scaled value of Score - average of Score of opponents.
             List<int> worstCaseScore = new List<int>(new int[hand.Count]);
@@ -43,7 +65,9 @@ namespace SixTakes
                     {
                         var newHand = new List<int>(hand);
                         newHand.RemoveAt(i);
-                        scoreDifference = MonteCarlo(newHand, newGame, depth - 1).Item2;
+                        // Scale down the number of iterations in the search tree.
+                        int newIterations = (int)Math.Sqrt(iterations);
+                        scoreDifference = MonteCarlo(newHand, newGame, depth - 1, newIterations).Item2;
                     }
                     worstCaseScore[i] = Math.Max(worstCaseScore[i], scoreDifference);
                 }
